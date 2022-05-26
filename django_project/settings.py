e8c4c45567b234a3f7098b59054baf81bc7aa15b
Amrit <f20211696@pilani.bits-pilani.ac.in>
2022-05-27 00:38:47 +0530

    Added django_on_heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from threading import local
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,4 +162,4 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
